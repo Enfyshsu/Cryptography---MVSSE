33f8f72d26e2bd77414f8a5c163602e24e8b88d8
chunyu618 <b06902048@ntu.edu.tw>
2021-06-10 16:41:11 +0800

    build keyword list

--- build_keywork.py
+++ build_keywork.py
@@ -5,19 +5,35 @@ def read_json(file_name):
         data = json.load(f)
     return data
 
+def write_json(file_name, doc):
+   with open(file_name, "w") as outfile:
+        json.dump(doc, outfile, indent=4, separators=(',', ': ')) 
+
 def main():
     words = {}
+    keyword = []
     file_name = 'Document.json'
     data = read_json(file_name)
     for d in data:
-        tmp = d['content'].split()
+        tmp = [i.strip().strip(',').strip('.').strip('(').strip(')') for i in d['content'].split()]
         for w in tmp:
-            if w. not in words.keys():
+            if w not in words.keys():
                 words[w] = 0
             words[w] += 1    
-    sorted(words.items(), key=lambda x: x[1])
+    #words = sorted(words.items(), key=lambda x: x[1], reverse=False)
+    cnt = 0
     for w in words:
-        print(w, words[w])
+        if words[w] >=5 and words[w] <= 10:
+            keyword.append(w)
+            cnt += 1
+    
+    for d in data:
+        d['keywords'] = []
+        for w in keyword:
+            if w in d['content']:
+                d['keywords'].append(w)
+    write_json("keyword_list.json", keyword) 
+    #write_json(file_name, data)
     
 if __name__ == '__main__':
     main()
