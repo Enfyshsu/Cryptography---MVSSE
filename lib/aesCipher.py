40a3d998059fc4c64269725e614bb4a91222a118
chunyu618 <b06902048@ntu.edu.tw>
2021-06-20 17:46:01 +0800

    use base64

--- lib/aesCipher.py
+++ lib/aesCipher.py
@@ -15,15 +15,15 @@ class AESCipher(object):
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        #return base64.b64encode(iv + cipher.encrypt(raw.encode()))
-        return iv + cipher.encrypt(raw.encode())
+        return base64.b64encode(iv + cipher.encrypt(raw.encode()))
+        #return iv + cipher.encrypt(raw.encode())
 
     def decrypt(self, enc):
-        #enc = base64.b64decode(enc)
+        enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        #return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
         return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        #return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
 
     def _pad(self, s):
         return s + (self.bs - len(s.encode()) % self.bs) * chr(self.bs - len(s.encode()) % self.bs)
@@ -36,8 +36,10 @@ def main():
     obj = AESCipher("abc")  
     enObj = obj.encrypt("This is \n data \n hahaha\n")
     print(enObj)
+    print(len(enObj))
     deObj = obj.decrypt(enObj)
     print(deObj)
+    print(len(deObj))
     
 
 if __name__ == '__main__':
