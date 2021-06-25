fb80e6521c96f0b3ba20301199873102256217e7
chunyu618 <b06902048@ntu.edu.tw>
2021-06-25 14:45:00 +0000

    Merge deletion to modification

--- owner_delete.py
+++
@@ -1,8 +0,0 @@
-from owner_modify import modify
-
-def main():
-    document_id = input("Please enter the document id you want to delete: ")
-    modify(document_id, "delete")
-
-if __name__ == "__main__":
-    main()
